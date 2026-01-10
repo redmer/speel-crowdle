e3587d0a6ff40e1d6672f67db679f75d98223184
Redmer Kronemeijer <redmer.kronemeijer@crow.nl>
2026-01-10 12:09:34 +0100

    feat: PWA

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/speel-crowdle/dev-sw.js?dev-sw', { scope: '/speel-crowdle/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import MyManifest from "./public/manifest.json";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      //@ts-expect-error
+      manifest: MyManifest,
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\//,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "external-api-cache",
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   base: "/speel-crowdle/",
 });
